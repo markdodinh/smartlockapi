44a91204cd4f01149f8d0dbf2f23b6f315f00a0b
Do Dinh <dinmark@amazon.com>
2017-06-15 21:37:32 -0700

    list api

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 open_requests = {}
 access_control_list = {
-    1 : 'user1'
+    1 : 'user1',
+    2 : 'user2',
+    3 : 'user3',
+    4 : 'user4',
+    5 : 'user5',
+    6 : 'user6'
 }
 
 @app.route('/open', methods = ['POST'])
@@ -31,3 +38,10 @@ def poll_open():
     locker = request.args.get('locker', None, int)
     username = open_requests.pop(locker, '')
     return jsonify(username = username)
+
+@app.route('/list', methods = ['GET'])
+def list():
+    arr = {}
+    for locker in access_control_list.keys():
+        arr['locker' + str(locker)] = locker in open_requests 
+    return jsonify(arr)
